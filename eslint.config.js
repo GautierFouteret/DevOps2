b2e3f9fa288d8a14fdde56bf4acc2bddd2235bcd
Gautier Fouteret <gautier.fouteret59@gmail.com>
2024-12-18 12:50:10 +0100

    mis a jour avec maj de eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,43 @@
 const pluginJest = require('eslint-plugin-jest');
 
-module.exports = [
-  {
-    // update this to match your test files
-    files: ['**/*.spec.js', '**/*.test.js'],
-    plugins: { jest: pluginJest },
-    languageOptions: {
-      globals: pluginJest.environments.globals.globals,
-    },
-    rules: {
-      'jest/no-disabled-tests': 'warn',
-      'jest/no-focused-tests': 'error',
-      'jest/no-identical-title': 'error',
-      'jest/prefer-to-have-length': 'warn',
-      'jest/valid-expect': 'error',
-    },
+module.exports = {
+  root: true,  // Assurez-vous que ce fichier est la racine de la configuration ESLint
+
+  // Spécifiez le parser et les options de parsing pour ESLint
+  parserOptions: {
+    ecmaVersion: 'latest',  // Assurez-vous de prendre en charge la dernière version ECMAScript
+    sourceType: 'module',   // Permet l'utilisation des modules ES
   },
-];
+
+  // Définissez les environnements globalement
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,  // La version d'ECMAScript que vous utilisez
+  },
+
+  overrides: [
+    {
+      // Appliquer ces règles aux fichiers de test spécifiés
+      files: ['**/*.spec.js', '**/*.test.js'],
+      plugins: { jest: pluginJest },
+      languageOptions: {
+        globals: pluginJest.environments.globals.globals,
+      },
+      rules: {
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'warn',
+        'jest/valid-expect': 'error',
+      },
+    },
+    {
+      // Pour le reste des fichiers (autres que les fichiers de test)
+      files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+      rules: {
+        // Ajoutez des règles générales ici, si nécessaire
+      },
+    },
+  ],
+};
